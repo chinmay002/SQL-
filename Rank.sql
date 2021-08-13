d5d5d01a76866294146dd2a20494912c881013cc
RedDevils <60249099+chinmay002@users.noreply.github.com>
2021-08-13 15:17:22 +0530

    Create Rank.sql

---
+++ Rank.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
++----+-------+
+| Id | Score |
++----+-------+
+| 1  | 3.50  |
+| 2  | 3.65  |
+| 3  | 4.00  |
+| 4  | 3.85  |
+| 5  | 4.00  |
+| 6  | 3.65  |
+
+
++----+-------+
+
+select score as Score,dense_rank() over(order by score desc) ' Rank' from scores
